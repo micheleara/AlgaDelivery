f4e4c46a88e7f40b0c86eaa5c508f2f3b38f84dd
Michele Oliveira <shellyguns@gmail.com>
2026-01-28 12:06:22 -0300

    Implementação do serviço de courier

--- Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/service/DeliveryPreparationService.java
+++ Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/service/DeliveryPreparationService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Duration;
 import java.util.UUID;
 
@@ -20,6 +21,8 @@ import java.util.UUID;
 public class DeliveryPreparationService {
 
     private final DeliveryRepository deliveryRepository;
+    private final DeliveryTimeEstimationService deliveryTimeEstimationService;
+    private final CourierPayoutCalculationService courierPayoutCalculationService;
 
     @Transactional
     public Delivery draft(DeliveryInput input){
@@ -59,17 +62,15 @@ public class DeliveryPreparationService {
                 .street(recipientInput.getStreet())
                 .build();
 
-        Duration expectedDeliveryTime = Duration.ofHours(3);
-        BigDecimal distanceFee = new BigDecimal("10");
+        DeliveryEstimate estimate = deliveryTimeEstimationService.estimate(sender, recipient);
+        BigDecimal calculatedPayout= courierPayoutCalculationService.calculatePayout(estimate.getDistanceInKm());
 
-
-        BigDecimal payout = new BigDecimal("10");
-
+        BigDecimal distanceFee = calculateFee(estimate.getDistanceInKm());
         var preparationsDetails = Delivery.PreparationsDetails.builder()
                 .recipient(recipient)
                 .sender(sender)
-                .expectedDeliveryTime(expectedDeliveryTime)
-                .courierPayout(payout)
+                .expectedDeliveryTime(estimate.getEstimatedTime())
+                .courierPayout(calculatedPayout)
                 .distanceFee(distanceFee)
                 .build();
 
@@ -80,4 +81,10 @@ public class DeliveryPreparationService {
         }
 
     }
+
+    private BigDecimal calculateFee(Double distanceInKm) {
+        return new BigDecimal("3").multiply(new BigDecimal(distanceInKm)).setScale(2, RoundingMode.HALF_EVEN);
+    }
 }
+
+
